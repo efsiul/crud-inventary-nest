b7d3b115089e5fc900d6dc15cae79fbf9b961dfa
moll-y <mol.tired@gmail.com>
2024-03-16 14:10:54 -0500

    feat: add global versioning and validation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Ensure all endpoints are protected from receiving incorrect data.
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableVersioning({ type: VersioningType.URI });
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
